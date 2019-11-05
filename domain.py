5e5b8abf1451dece41f92b3d88096663b3fadb14
Dangote <jaotjoseph4@gmail.com>
2019-11-05 18:04:05 +0100

    initial commit

---
+++ domain.py
@@ -0,0 +1,8 @@
+import requests
+
+# Authenticcation
+username =  "timothyolaleke@gmail.com"
+apikey = "78a3dd201c6b79abaeb26e02f8801ad8"
+timeStamp = 1572969306
+hash = "db073ef1c237fe98ae1b3aec8ba3a2b8"
+auth = timeStamp + ‘:’ + hash
